2a5c1dd7e2d7c5c471a27c13ff7e7823c3a15b32
Wilson Cheng <wilzon@gmail.com>
2015-04-20 11:10:32 +0800

    working version Apr 1110

--- js/application.js
+++ js/application.js
@@ -12,6 +12,14 @@ $(document).ready(function() {
   logOutRequest();
   });
   
+  $(document).on('click','.tweet-button', function(){
+  tweetRequest();
+  });
+  
+  $(document).on('click','.list-all-button', function(){
+  listRequest();
+  });
+  
   // $(document).on('click','.listButton', function(){
   //   listRequest();
   // });
@@ -35,11 +43,18 @@ $(document).ready(function() {
       dataType: "JSON",
       success: function(response) {
         console.log("Great success, user signed up", response);
-        $('.signup-username').val("nice");
-        $('.signup-email').val("awesome");
-        $('.signup-password').val("cool");
-        }
+        // $('.signup-username').val("nice");
+        // $('.signup-email').val("awesome");
+        // $('.signup-password').val("cool");
+        alert("Sign up successful!");
+        window.location = "/tweetpage.html";
+        postLoginStatus();
+      }
     }) 
+  }
+
+  function postLoginStatus() {
+    $('.loginStatus').append("<p>Status: Logged in</p>");
   }
 
 //login request
@@ -59,8 +74,51 @@ $(document).ready(function() {
       },
       success: function(response) {
        console.log("Great success, user logged in.", response);
-        $('.login-username').val("nice");
-        $('.login-password').val("awesome");
+        //$('.login-username').val("nice");
+        //$('.login-password').val("awesome");
+        window.location = "/tweetpage.html";
+        listRequest();
+        postLoginStatus();
+
+      }
+    }) 
+  }
+
+//post request
+  function tweetRequest() {
+    $.ajax({
+      type: "POST",
+      url: 'http://localhost:3000/tweets',
+      data: {
+        tweet: {
+          message:  $('.newTweet').val(),
+        }      
+      },
+      xhrFields: { //xhrFields is for necessary for cross domain and probably cross ports.
+      withCredentials: true
+      },
+      dataType: "JSON",
+      success: function(response) {
+        console.log("Great success", response);
+        $('.newTweet').val("");
+ //       $('.newAuthor').val("");
+        listRequest();
+        }
+    }) 
+  }
+
+  //list request ****************************************************************
+  function listRequest() {
+    $.ajax({
+      type: "GET",
+      url: 'http://localhost:3000/tweets',
+      dataType: "JSON",
+      success: function(response) {
+        console.log("Great listing success", response);
+        $('#all-posts > tbody').text(''); 
+        response.forEach(function (post) {
+          $('#all-posts > tbody').append("<tr class='post-item'>" + "<td>" + post._id + "</td>" + "<td>" + post.message + "</td>"+ "</tr>" );
+        });
       }
     }) 
   }
@@ -82,6 +140,8 @@ $(document).ready(function() {
       // },
       success: function(response) {
        console.log("Great success, user logged OUT.", response);
+       window.location = "/index.html";
+
       }
      // }
     })
